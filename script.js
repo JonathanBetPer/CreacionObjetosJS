549cbf25271097874441451f737cb3dbe5376731
JonathanB <jonathanbetancorperdomo@gmail.com>
2023-02-27 18:32:53 +0000

    script editado
    JS cambiado para que funcione con mi JSON

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
 
-const requestURL = './objetosDiagramaClases.json';
+const requestURL = './objetosDiagramClases.json';
 
 const request = new XMLHttpRequest();
 
@@ -11,49 +11,40 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-  const superHeroes = request.response;
-  populateHeader(superHeroes);
-  showHeroes(superHeroes);
+  const desarrolladora = request.response;
+  populateHeader(desarrolladora);
+  showInfo(desarrolladora);
 }
 
 function populateHeader(jsonObj) {
   const myH1 = document.createElement('h1');
-  myH1.textContent = jsonObj['squadName'];
+  myH1.textContent = jsonObj['desarrolladora'];
   header.appendChild(myH1);
 
   const myPara = document.createElement('p');
-  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+  myPara.textContent = 'Email: ' + jsonObj['email'] + ' // WEB: ' + jsonObj['link-web'];
   header.appendChild(myPara);
 }
 
-function showHeroes(jsonObj) {
-  const heroes = jsonObj['members'];
+function showInfo(jsonObj) {
+  const proyectos = jsonObj['proyectos'];
 
-  for (var i = 0; i < heroes.length; i++) {
+  for (var i = 0; i < proyectos.length; i++) {
     const myArticle = document.createElement('article');
     const myH2 = document.createElement('h2');
     const myPara1 = document.createElement('p');
     const myPara2 = document.createElement('p');
     const myPara3 = document.createElement('p');
-    const myList = document.createElement('ul');
 
-    myH2.textContent = heroes[i].name;
-    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-    myPara2.textContent = 'Age: ' + heroes[i].age;
-    myPara3.textContent = 'Superpowers:';
-
-    const superPowers = heroes[i].powers;
-    for (var j = 0; j < superPowers.length; j++) {
-      const listItem = document.createElement('li');
-      listItem.textContent = superPowers[j];
-      myList.appendChild(listItem);
-    }
+    myH2.textContent = proyectos[i].nombre;
+    myPara1.textContent = 'id: ' + proyectos[i].id;
+    myPara2.textContent = 'descripcion: ' + proyectos[i].descipcion;
+    myPara3.textContent = 'numeroVacantes: ' + proyectos[i].numeroVacantes;
 
     myArticle.appendChild(myH2);
     myArticle.appendChild(myPara1);
     myArticle.appendChild(myPara2);
     myArticle.appendChild(myPara3);
-    myArticle.appendChild(myList);
 
     section.appendChild(myArticle);
   }
@@ -66,8 +57,8 @@ request.responseType = 'text'; // recibimos una cadena de tipo "string"
 request.send();
 
 request.onload = function() {
-  const superHeroesText = request.response; // cogemos la cadena de response
-  const superHeroes = JSON.parse(superHeroesText); // la convertimos a objeto
-  populateHeader(superHeroes);
-  showHeroes(superHeroes);
+  const TextoDesarrolladora = request.response; // cogemos la cadena de response
+  const desarrolladora = JSON.parse(TextoDesarrolladora); // la convertimos a objeto
+  populateHeader(desarrolladora);
+  showInfo(desarrolladora);
 }
